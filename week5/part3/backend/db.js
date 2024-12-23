484ffdc53925a6f10c32ac9b6160d85cbd66a2a6
GoodGuyPJ <workwithgoodguypj@gmail.com>
2024-12-23 22:07:42 +0530

    react

---
+++ week5/part3/backend/db.js
@@ -0,0 +1,23 @@
+/*
+Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+  } 
+*/
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+
+mongoose.connect('mongodb_url');
+
+const todoSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean
+})
+
+const todo = mongoose.model('todos', todoSchema);
+module.exports = {
+  todo
+}
