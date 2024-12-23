0e5d6510de30ec2e4d739578d7d7add7e68cfb04
GoodGuyPJ <workwithgoodguypj@gmail.com>
2024-12-23 16:39:01 +0530

    add react todo

--- week5/part3/backend/index.js
+++ week5/part3/backend/index.js
@@ -1,7 +1,5 @@
-//write basic express boilerplate code,
-//with express.json() middleware
-
 const express = require('express');
+const { createTodo } = require('./types');
 const app = express();
 
 app.use(express.json());
@@ -12,8 +10,15 @@ app.use(express.json());
 //}
 
 app.post("/todo", function(req, res) {
-    console.log(req.body);
-    
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success) {
+      res.status(411).json({
+        msg: "You sent the wrong inputs",
+      })
+      return;
+    }
+    //put it in mongoDB
 });
 
 app.get("/todos", function(req, res) { 
@@ -21,7 +26,14 @@ app.get("/todos", function(req, res) {
 });
 
 app.put("/completed", function(req, res) {
-    
+    const updatePayload = req.body;
+    const paresePayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success) {
+      res.status(411).json({
+        msg: "You sent the wrong inputs",
+      })
+      return;
+    }
 })
 
 app.listen(3000, () => {
