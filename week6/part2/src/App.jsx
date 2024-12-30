11970018de5d44652b1f8177cfacaf5ac45fa230
GoodGuyPJ <workwithgoodguypj@gmail.com>
2024-12-30 13:53:49 +0530

    wrapper react

--- week6/part2/src/App.jsx
+++ week6/part2/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Children } from "react";
 
 function App() {
   return (
     <div>
-      <CardWapper innerComponent={<TextComponent />} /> <br />
-      <CardWapper innerComponent={<TextComponent2 />} />
+      {/* <CardWapper innerComponent={<TextComponent />} /> <br />
+      <CardWapper innerComponent={<TextComponent2 />} /> */}
+      <CardWapper>hi there</CardWapper>
     </div>
   );
 }
@@ -19,11 +21,12 @@ function TextComponent2() {
   return <div> no hi there</div>;
 }
 
-function CardWapper({ innerComponent }) {
+function CardWapper({ children }) {
   return (
-    <div style={{ border: "2px solid red", padding: "15px" }}>
-      {innerComponent}
+    <div style={{ border: "2px solid red", padding: 20 }}>
+      {/* {innerComponent} */}
       {/* {props.innerComponent()} */}
+      {children}
     </div>
   );
 }
