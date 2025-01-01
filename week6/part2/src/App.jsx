a7ced4a7fa3d9a2da1deecf79f01286ac62db82e
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-01-01 15:19:26 +0530

    commit

--- week6/part2/src/App.jsx
+++ week6/part2/src/App.jsx
@@ -1,34 +1,69 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Children } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://sum-server.100devs.com/todos").then(async function (res) {
+      const json = await res.json();
+      setTodos(json);
+    });
+  }, []);
+
   return (
     <div>
-      {/* <CardWapper innerComponent={<TextComponent />} /> <br />
-      <CardWapper innerComponent={<TextComponent2 />} /> */}
-      <CardWapper>hi there</CardWapper>
+      {todos.map((todo) => (
+        <Todo key={todo.id} title={todo.title} description={todo.description} />
+      ))}
     </div>
   );
 }
 
-function TextComponent() {
-  return <div>hi there</div>;
-}
-function TextComponent2() {
-  return <div> no hi there</div>;
-}
-
-function CardWapper({ children }) {
-  return (
-    <div style={{ border: "2px solid red", padding: 20 }}>
-      {/* {innerComponent} */}
-      {/* {props.innerComponent()} */}
-      {children}
-    </div>
-  );
+function Todo({ title, description }) {
+  <div>
+    <h2>{title}</h2>
+    <h3>{description}</h3>
+  </div>;
 }
 
 export default App;
+
+/* 
+  // import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import { Children } from "react";
+
+// function App() {
+//   return (
+//     <div>
+//       {/* <CardWapper innerComponent={<TextComponent />} /> <br />
+//       <CardWapper innerComponent={<TextComponent2 />} /> }
+//       <CardWapper>hi there</CardWapper>
+//     </div>
+//   );
+// }
+
+// function TextComponent() {
+//   return <div>hi there</div>;
+// }
+// function TextComponent2() {
+//   return <div> no hi there</div>;
+// }
+
+// function CardWapper({ children }) {
+//   return (
+//     <div style={{ border: "2px solid red", padding: 20 }}>
+//       {/* {innerComponent} }
+//       {/* {props.innerComponent()}  }
+//       {children}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// */
