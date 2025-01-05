ba5ea4d33f928027cadceb82eadb1a76b94d417a
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-01-05 21:06:06 +0530

    useEffect React

--- week6/part3/src/App.jsx
+++ week6/part3/src/App.jsx
@@ -3,21 +3,65 @@ import { useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  return (
+    <div>
+      <Todo id={3} />
+    </div>
+  );
+}
+// const [todos, setTodos] = useState([]);
+
+// useEffect(() => {
+//   axios.get("http://localhost:8000/todos").then(function (res) {
+//     setTodos(res.data.todos);
+//   });
+// }, []);
+
+//to show id title and description using useEffect
+function Todo({ id }) {
+  const [todo, setTodo] = useState({});
 
   useEffect(() => {
-    axios.get("http://localhost:8000/todos").then(function (res) {
-      setTodos(res.data.todos);
-    });
-  }, []);
-
-  //to show id title and description using useEffect
-  useEffect(() => {
-    axios.get(`http://localhost:8000/todo?id={$id}`).then(function (res) {
+    axios.get(`http://localhost:8000/todo?id=` + id).then((res) => {
       console.log(res.data.todo);
-      setTodos(res.data.todo);
+      setTodo(res.data.todo);
     });
   }, []);
+
+  return (
+    <div>
+      <h2>{todo.title}</h2>
+      <h2>{todo.description}</h2>
+    </div>
+  );
+}
+
+// function Todo({ title, description }) {
+//   return (
+//     <div>
+//       <h2>{title}</h2>
+//       <h3>{description}</h3>
+//     </div>
+//   );
+// }
+
+export default App;
+
+/*  
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios";
+
+function App() {
+   const [todos, setTodos] = useState([]);
+
+
+  // useEffect(() => {
+  //   axios.get("http://localhost:8000/todos").then(function (res) {
+  //     setTodos(res.data.todos);
+  //   });
+  // }, []);
+
 
   return (
     <div>
@@ -38,3 +82,4 @@ function Todo({ title, description }) {
 }
 
 export default App;
+*/
