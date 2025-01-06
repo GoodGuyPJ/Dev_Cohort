6930774dfebcd256f44b0eaf640f854c99597176
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-01-06 22:21:50 +0530

    useCallback and memo

--- week6/part4/vite-project/src/App.jsx
+++ week6/part4/vite-project/src/App.jsx
@@ -68,3 +68,41 @@ function App() {
 }
 
 export default App;
+
+
+
+// useCallback and useMemo
+import { memo, useCallback, useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const inputFunction = useCallback(() => {
+    console.log("hi there");
+  }, []);
+
+  return (
+    <div>
+      <ButtonComponent inputFunction={inputFunction} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me {count}
+      </button>
+    </div>
+  );
+}
+
+const ButtonComponent = memo(({ inputFunction }) => {
+  console.log("child render");
+
+  return (
+    <div>
+      <button>Click me</button>
+    </div>
+  );
+});
+
+export default App;
