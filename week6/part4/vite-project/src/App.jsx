2d063864fdaaccb90146d8e56bae4c0f602d1c52
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-01-06 19:23:48 +0530

    useMemo

---
+++ week6/part4/vite-project/src/App.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useMemo, useState } from "react";
+/**
+   Create an app that does two things:
+   1. Increase a counter by 1
+   2. lets user put a value in an input box(n) and you need to show sum form 1-n.
+ */
+
+//useMemo is used to memoize the value of count so that it does not get recalculated on every render
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+  const [finalValue, setFinalValue] = useState(0);
+
+  useEffect(() => {
+    let count = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      count = count + i;
+    }
+    setFinalValue(count);
+  }, [inputValue]);
+
+  let count = useMemo(() => {
+    let count = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      count += i;
+    }
+    return count;
+  }, [inputValue]);
+
+  // let count = 0;
+  // for (let i = 1; i <= inputValue; i++) {
+  //   count += i;
+  // }
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={function (e) {
+          setInputValue(e.target.value);
+        }}
+        placeholder="Enter a number to find sum from 1 to n"
+      />{" "}
+      <br />
+      sum from 1 to {inputValue} is {count}
+      <br />
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Counter ({counter}){" "}
+      </button>
+    </div>
+  );
+}
+
+export default App;
