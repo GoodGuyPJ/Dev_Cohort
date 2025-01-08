881661886cbe55d55d9fe379248538a8425ffe7b
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-01-08 18:22:13 +0530

    react recap

--- week6/part5/src/App.jsx
+++ week6/part5/src/App.jsx
@@ -3,18 +3,36 @@ import axios from "axios";
 import { useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [exchangeData, setExchangeData] = useState({});
+  const [bankData, setBankData] = useState({});
+
+  // fetch("https://api.exchangeratesapi.io/latest", async (res) => {
+  //   const json = await res.json();
+  //   setExchangeData(json);
+  // })
+
+  setTimeout(() => {
+    setBankData({
+      income: 1000,
+    });
+  }, 3000);
+
+  setTimeout(() => {
+    setExchangeData({
+      returns: 100,
+    });
+  }, 1000);
+
+  const incomeTax = (bankData.income + (exchangeData.returns || 0)) * 0.3;
 
   return (
-    <>
-      <button
-        onClick={function () {
-          setCount(count + 1);
-        }}
-      >
-        Count is {count}
-      </button>
-    </>
+    <div>
+      <h1>Bank</h1>
+      <p>Income: {bankData.income}</p>
+      <p>Income tax: {incomeTax}</p>
+      <h1>Exchange</h1>
+      <p>Exchange returns: {exchangeData.returns}</p>
+    </div>
   );
 }
 
