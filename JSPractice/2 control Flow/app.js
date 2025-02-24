3f9ac0a5edbe61b0fb997370551dae8b26b0ea39
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-02-24 17:55:46 +0530

    commit

--- JSPractice/2 control Flow/app.js
+++ JSPractice/2 control Flow/app.js
@@ -3,11 +3,10 @@
 //   "banana",
 //   "cherry",
 //   "date",
-//   "elderberry", 
+//   "elderberry",
 //   "fig",
 // ];
 // fruit.push("grape");
-
 
 // const  pl = ["javascript", "on", "ruby", "php", "java", "c"];
 // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -22,7 +21,6 @@
 // const greeting = function (user) {
 //   console.log(`Hello, ${user}`);
 // }
-
 
 // const person = {
 //   name: "John",
@@ -46,7 +44,6 @@
 //   }
 // }
 
-
 // const jsonString = JSON.stringify(person2)
 // const parsedObject = JSON.parse(jsonString);
 
@@ -56,8 +53,8 @@
 // const date = new Date();
 
 // const year = date.getFullYear();
-// const month = date.getMonth();  
-// const day = date.getDate(); 
+// const month = date.getMonth();
+// const day = date.getDate();
 // const hours = date.getHours();
 
 // console.log(`Date: ${year}, Date: ${month}, Date: ${day}, Date: ${hours}`);
@@ -79,89 +76,220 @@
 //   clearInterval(intervalId);
 // }, 5000);
 
+// setTimeout( () => {
+//   console.log("hello");
+//   setTimeout( () => {
+//     console.log("world");
+//     setTimeout(() =>{
+//       console.log("namaste");
+//       setTimeout(() => {
+//         console.log("salam");
+//       }, 2000)
+//     }, 2000)
+//   }, 2000)
+// }, 2000)
 
-setTimeout( () => {
-  console.log("hello");
-  setTimeout( () => {
-    console.log("world");
-    setTimeout(() =>{
-      console.log("namaste");
-      setTimeout(() => {
-        console.log("salam");
-      }, 2000)
-    }, 2000)
-  }, 2000)
-}, 2000)
+// const strNums = ["one", "two", "three", "four", "five"];
+// const moreStrNums = ["six", "seven", "eight", "nine", "ten"];
 
-const strNums = ["one", "two", "three", "four", "five"];
-const moreStrNums = ["six", "seven", "eight", "nine", "ten"];
+// const concat = [...strNums, ...moreStrNums];
+// console.log(concat);
 
-const concat = [...strNums, ...moreStrNums];
-console.log(concat);
+// let peoples = ['huxn', 'john', 'doe', 'jane', 'smith'];
+// console.log('kumar', ...peoples);
 
-let peoples = ['huxn', 'john', 'doe', 'jane', 'smith'];
-console.log('kumar', ...peoples);
+// let person = {
+//   name: 'john',
+//   age: 25,
+//   email: 'john.gmail.com'
+// }
 
-let person = {
-  name: 'john',
-  age: 25,
-  email: 'john.gmail.com'
-}
+// const clone = {...person, works: "Programmer", location: 'locations'};
+// console.log(clone);
 
-const clone = {...person, works: "Programmer", location: 'locations'};
-console.log(clone);
+//  function sum(a, b, ...hobbies) {
+//    console.log(a, b, hobbies);
+//  }
+//  sum('a', 'b', 'c', 'd', 'e', 'f', 'g');
 
- function sum(a, b, ...hobbies) {
-   console.log(a, b, hobbies);
- }
- sum('a', 'b', 'c', 'd', 'e', 'f', 'g');
+//  const foo = ["one", "two", "three", "four", "five"];
 
- const foo = ["one", "two", "three", "four", "five"];
+//  const [three] = foo;
+//  console.log(three);
 
- const [three] = foo;
- console.log(three);
+//  function f() {
+//     return [1, 2, 3];
+//  }
 
- function f() {
-    return [1, 2, 3];
- }
+//  let a, b
+//  [a, b] = f()
 
- let a, b 
- [a, b] = f()
+//  function person(firstName, lastName, ...hobbies) {
+//    console.log(firstName, lastName, hobbies);
+//  }
+//  person('john', 'doe', 'reading', 'coding', 'gaming');
 
- function person(firstName, lastName, ...hobbies) {
-   console.log(firstName, lastName, hobbies);
- }
- person('john', 'doe', 'reading', 'coding', 'gaming');
+// //  complete learing
 
-//  complete learing
+// typeof person;
 
-typeof person;
+// const data =  {
+//   user: {
+//     id: 123,
+//     name: "john",
+//     age: 25,
+//     email: "john.doe@example.com",
+//     address: {
+//       street: "123 main st",
+//       city: "New York"
+//     }
+//   }
+// }
 
-const data =  {
-  user: {
-    id: 123,
-    name: "john",
-    age: 25,
-    email: "john.doe@example.com",
-    address: {
-      street: "123 main st",
-      city: "New York"
-    } 
+// let list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for(let index in list){
+//   console.log(`${index}: ${list[index]}`);
+// }
+
+// const colors = ["red", "green", "blue", "yellow", "orange"];
+// colors.forEach((color) => console.log(color));
+
+// const words = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+// const capWords = words.forEach((word, index, arr) =>{
+//   arr[index] = word.toUpperCase() + word.substring(1);
+// });
+
+// console.log(words)
+
+// const songs = [
+//   {name: "song1", duration: 3.45},
+//   {name: "song2", duration: 4.45},
+//   {name: "song3", duration: 5.45},
+//   {name: "song4", duration: 6.45},
+// ];
+
+// console.log(songs.filter((song) => song.duration > 5));
+
+// const computer = [
+//   {name: "apple", price: 1000},
+//   {name: "dell", price: 800},
+//   {name: "hp", price: 700},
+//   {name: "asus", price: 600},
+// ]
+// console.log(computer.filter((com) => com.price > 700));
+
+// const peoples = [
+//   {name: "john", age: 25},
+//   {name: "doe", age: 30},
+//   {name: "jane", age: 35},
+//   {name: "smith", age: 40},
+// ];
+
+// const res = peoples.find(person => person.name === "doe");
+// console.log(res);
+
+//every() method
+const peoples = ["john", "doei", "jane", "smit"];
+
+const res1 = peoples.every((people) => people.length === 4);
+console.log(res1);
+
+//some() method
+const res2 = peoples.some((people) => people.length > 3);
+console.log(res2);
+
+//Refactor -> use every and some helpers
+let products = [
+  { name: "Checkers", category: "Toys" },
+  { name: "Chess", category: "Books" },
+  { name: "Bike", category: "Vehicle" },
+  { name: "Car", category: "Vehicle" },
+];
+
+let allProductsBooks = true;
+let someProductsBooks = false;
+
+for (let i = 0; i < products.length; i++) {
+  if (products[i].category !== "Books") {
+    allProductsBooks = false;
+  } else {
+    someProductsBooks = true;
   }
 }
 
-let list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-for(let index in list){
-  console.log(`${index}: ${list[index]}`);
-}
+//do all products have a category of books?
+// let allProductsBooks = products.every((product) => product.category === "Books");
 
-const colors = ["red", "green", "blue", "yellow", "orange"];
-colors.forEach((color) => console.log(color));
+console.log(allProductsBooks);
+console.log(someProductsBooks);
 
-const words = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+//do some products have a category of books?
+// let someProductsBooks = products.some((product) => product.category === "Books");
 
-const capWords = words.forEach((word, index, arr) =>{
-  arr[index] = word.toUpperCase() + word.substring(1);
+// reduce() method
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const sum = numbers.reduce((acc, num) => acc+num, 0);
+const sum = numbers.reduce((acc, num) => {
+  console.log(acc, num);
+  return acc + num;
+}, 0);
+console.log(sum);
+
+const peoples1 = [
+  {
+    name: "john",
+    age: 25,
+  },
+  {
+    name: "doe",
+    age: 30,
+  },
+  {
+    name: "jane",
+    age: 35,
+  },
+  {
+    name: "smith",
+    age: 40,
+  },
+];
+
+const res = peoples1.reduce(
+  (acc, people) => (people.age > acc ? people.age : acc),
+  0
+);
+console.log(res);
+
+const fruit = ["apple", "banana", "cherry", "date", "elderberry", "fig"];
+
+const wordFrequency = fruit.reduce((frequencyMap, word) => {
+  frequencyMap[word] = (frequencyMap[word] || 0) + 1;
+  return frequencyMap;
 });
+console.log(wordFrequency);
 
-console.log(words)
+const numbers1 = [1, 2, 3, 4];
+
+const calculateProduct = (numbers) => {
+  return numbers.reduce((acc, num) => acc * num, 1);
+};
+
+const product = calculateProduct(numbers1);
+console.log(product);
+
+//Map() method
+const map = new Map();
+
+const keyOne = "string";
+const keyTwo = {};
+const keyThree = function () {};
+
+map.set(keyOne, "value associated with string");
+map.set(keyTwo, "value associated with object");
+map.set(keyThree, "value associated with function");
+console.log(map);
+
+console.log(map.keys());
+console.log(map.values());
+console.log(map.delete(keyThree));
