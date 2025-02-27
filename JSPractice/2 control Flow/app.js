22ccc1147dcee805931020fd710597f27b8e257e
GoodGuyPJ <workwithgoodguypj@gmail.com>
2025-02-27 12:11:39 +0530

    learning JS

--- JSPractice/2 control Flow/app.js
+++ JSPractice/2 control Flow/app.js
@@ -440,12 +440,44 @@
 //   emoji.innerHTML = emojis[Math.floor((Math.random() * emojis.length) | 0)];
 // });
 
-const btns = document.querySelectorAll(".btn");
-const body = document.body;
+// const btns = document.querySelectorAll(".btn");
+// const body = document.body;
 
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    number = btn.value;
-    changeBackground(number);
-  });
-});
+// btns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     number = btn.value;
+//     changeBackground(number);
+//   });
+// });
+
+// function changeBackground(number) {
+//   // body.style.backgroundColor = `rgb(${Math.random() * 255}, ${
+//   //   Math.random() * 255
+//   // }, ${Math.random() * 255})`;
+
+//   body.className = "";
+
+//   // switch (number) {
+//   //   case "purple":
+//   //     body.classList.add("purple");
+//   //     break;
+//   //   case "blue":
+//   //     body.classList.add("blue");
+//   //     break;
+//   //   case "green":
+//   //     body.classList.add("green");
+//   //     break;
+//   //   case "yellow":
+//   //     body.classList.add("yellow");
+//   //     break;
+//   //   case "orange":
+//   //     body.classList.add("orange");
+//   //     break;
+//   //   case "red":
+//   //     body.classList.add("red");
+//   //     break;
+//   //   case "pink":
+//   //     body.classList.add("pink");
+//   //     break;
+//   // }
+// }
